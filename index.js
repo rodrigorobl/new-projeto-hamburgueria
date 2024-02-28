927398636ff7546f3cea615a7fd363558644a17e
rodrigorobl <rodrigo.augusto.r21@gmail.com>
2024-02-28 17:52:24 -0300

    jjjjjjjjjjj

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const uuid = require('uuid')
-const port = 3000
+const cors = require('cors')
+
+const port = 3001
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 const orders = []
 
